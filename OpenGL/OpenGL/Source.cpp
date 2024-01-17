4d5b832969bc3f14bf79868591d7946f84db1b89
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2024-01-18 00:05:46 +0700

    bug render mau den

--- OpenGL/OpenGL/Source.cpp
+++ OpenGL/OpenGL/Source.cpp
@@ -47,13 +47,130 @@ int main() {
 		return -1;
 	}
 
-	stbi_set_flip_vertically_on_load(true);
-
 	glEnable(GL_DEPTH_TEST);
 
-	Shader ourShader("vShader.vert", "fShader.frag");
+    float cubeVertices[] = {
+        // positions          // texture Coords
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
 
-	Model backpack("D:\\OpenGL\\OpenGL\\OpenGL\\backpack\\backpack.obj");
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+         0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+         0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+        -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+    };
+    float planeVertices[] = {
+        // positions          // texture Coords (note we set these higher than 1 (together with GL_REPEAT as texture wrapping mode). this will cause the floor texture to repeat)
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f,  5.0f,  0.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+
+         5.0f, -0.5f,  5.0f,  2.0f, 0.0f,
+        -5.0f, -0.5f, -5.0f,  0.0f, 2.0f,
+         5.0f, -0.5f, -5.0f,  2.0f, 2.0f
+    };
+
+    unsigned int cubeVBO, cubeVAO;
+    unsigned int planeVBO, planeVAO;
+
+    glGenBuffers(1, &cubeVBO);
+    glGenVertexArrays(1, &cubeVAO);
+    glBindVertexArray(cubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    glGenBuffers(1, &planeVBO);
+    glGenVertexArrays(1, &planeVAO);
+    glBindVertexArray(planeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 5, (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(float) * 5, (void*)(sizeof(float) * 3));
+    glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned int metal, marble;
+    unsigned char* metalData, * marbleData;
+    int metalWidth, metalHeight, marbleWidth, marbleHeight;
+    int metalNRChannels, marbleNRChannels;
+    GLenum colorFormat = GL_RGB;
+
+    glGenTextures(1, &metal);
+    metalData = stbi_load("metal.png", &metalWidth, &metalHeight, &metalNRChannels, 0);
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, metal);
+    if (metalNRChannels == 4)
+        colorFormat = GL_RGBA;
+    glTexImage2D(GL_TEXTURE_2D, 0, colorFormat, metalWidth, metalHeight, 0, colorFormat, GL_UNSIGNED_BYTE, metalData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    stbi_image_free(metalData);
+    colorFormat = GL_RGB;
+
+    glGenTextures(1, &marble);
+    marbleData = stbi_load("marble.jpg", &marbleWidth, &marbleHeight, &marbleNRChannels, 0);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, marble);
+    if (metalNRChannels == 4)
+        colorFormat = GL_RGBA;
+    glTexImage2D(GL_TEXTURE_2D, 0, colorFormat, marbleWidth, marbleHeight, 0, colorFormat, GL_UNSIGNED_BYTE, marbleData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    stbi_image_free(marbleData);
+
+    Shader ourShader("vShader.vert", "fShader.frag");
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600, 0.1f, 100.0f);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -63,44 +180,47 @@ int main() {
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		ourShader.use();
+        ourShader.use();
 
-		//20, 1.0, 0.22, 0.20
-		ourShader.setVec3("lights[1].position", glm::vec3(0.0f, 0.0f, 2));
-		ourShader.setVec3("lights[1].ambient", glm::vec3(0.3f, 0.3f, 0.3f));
-		ourShader.setVec3("lights[1].diffuse", glm::vec3(0.7f, 0.7f, 0.7f));
-		ourShader.setVec3("lights[1].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		ourShader.setFloat("lights[1].constant", 1);
-		ourShader.setFloat("lights[1].linear", 0.22);
-		ourShader.setFloat("lights[1].quadratic", 0.20);
-		ourShader.setFloat("lights[1].shininess", 128.0f);
+        glm::mat4 view = camera.calculateView();
+        ourShader.setMat4("projection", projection);
+        ourShader.setMat4("view", view);
 
-		ourShader.setVec3("lights[0].position", glm::vec3(0.0f, 0.0f, -2));
-		ourShader.setVec3("lights[0].ambient", glm::vec3(0.3f, 0.3f, 0.3f));
-		ourShader.setVec3("lights[0].diffuse", glm::vec3(0.6f, 0.6f, 0.6f));
-		ourShader.setVec3("lights[0].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		ourShader.setFloat("lights[0].constant", 1);
-		ourShader.setFloat("lights[0].linear", 0.22);
-		ourShader.setFloat("lights[0].quadratic", 0.20);
-		ourShader.setFloat("lights[0].shininess", 128.0f);
+        //render cube 1
+        glBindVertexArray(cubeVAO);
+        glm::mat4 model(0.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.25f, 0.0f));
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, metal);
+        ourShader.setInt("Image", 0);
+        ourShader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		ourShader.setVec3("cameraFront", camera.getCameraDirection());
+        //render cube 2
+        model = glm::translate(model, glm::vec3(2.0f, 0.25f, -2.0f));
+        ourShader.setMat4("model", model);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glBindVertexArray(0);
 
-		glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
-		glm::mat4 view = camera.calculateView();
-		ourShader.setMat4("projection", projection);
-		ourShader.setMat4("view", view);
+        //render plane
+        glBindVertexArray(planeVAO);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
+        ourShader.setMat4("model", model);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, marble);
+        ourShader.setInt("Image", 1);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        glBindVertexArray(0);
 
-		glm::mat4 model = glm::mat4(1.0f);
-		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f)); // translate it down so it's at the center of the scene
-		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));	// it's a bit too big for our scene, so scale it down
-		ourShader.setMat4("model", model);
-		backpack.Draw(ourShader);
-
-		ourShader.unuse();
+        ourShader.unuse();
 
 		glfwSwapBuffers(window);
 	}
+
+    glDeleteBuffers(1, &planeVBO);
+    glDeleteBuffers(1, &cubeVBO);
+    glDeleteVertexArrays(1, &planeVAO);
+    glDeleteVertexArrays(1, &cubeVAO);
 
 	glfwTerminate();
 
--- OpenGL/OpenGL/fShader.frag
+++ OpenGL/OpenGL/fShader.frag
@@ -1,79 +1,12 @@
 #version 330 core
 
-struct Material {
-	sampler2D texture_diffuse0; //might be changed to 0
-	sampler2D texture_specular0;
-};
-
-struct PointLight {
-	vec3 position;
-
-	vec3 ambient;
-	vec3 diffuse;
-	vec3 specular;
-	float shininess;
-
-	float constant;
-	float linear;
-	float quadratic;
-};
-
-in vec2 TexCoords;
-in vec3 Normal;
-in vec3 FragPos;
-
-uniform Material material;
-uniform PointLight lights[2];
-uniform vec3 cameraFront;
-
 out vec4 FragColor;
 
-float calculatePointLight_Attenuation(PointLight light) {
-	float distance    = length(light.position - FragPos);
-	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+in vec2 TexCoords;
 
-	return attenuation;
-}
-
-vec4 calculatePointLight_AmbientDiffuse(PointLight light) {
-	vec3 ambient = light.ambient;
-
-	float diffuseFactor = max(dot(normalize(Normal), normalize(light.position - FragPos)), 0.0f);
-
-	vec3 diffuse = light.diffuse * diffuseFactor;
-
-	if (diffuseFactor > 0)
-		diffuse *= calculatePointLight_Attenuation(light);
-
-	vec4 amDifResult = vec4(ambient + diffuse, 1.0f) * texture(material.texture_diffuse0, TexCoords);
-
-	return amDifResult;
-}
-
-vec4 calculatePointLight_Specular(PointLight light) {
-	vec3 reflectedLight =  reflect(normalize(FragPos - light.position), normalize(Normal));
-	float specularIntensity = max(dot(normalize(reflectedLight), -normalize(cameraFront)), 0.0f);
-	float specularFactor = pow(specularIntensity, light.shininess);
-
-	vec3 specular = light.specular * specularFactor; 
-
-	vec4 specResult = vec4(specular, 1.0f) * texture(material.texture_specular0, TexCoords);
-
-	if (specularIntensity > 0) 
-		specResult *= calculatePointLight_Attenuation(light);
-
-	return specResult;
-}
-
+uniform sampler2D Image;
 
 void main() {
 
-	vec4 result = vec4(0.0f);
-
-	for (int i = 0; i < 2; i++) {
-		result += calculatePointLight_AmbientDiffuse(lights[i])
-				+ calculatePointLight_Specular(lights[i]);
-	}
-
-	FragColor = result;
+	FragColor = texture(Image, TexCoords);
 }
