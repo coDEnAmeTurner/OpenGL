781d78ed7af7cb0dd0db7bd662b8adb9c2607fa2
coDEnAmeTurner <tranluuquoctuan191191@gmail.com>
2024-01-21 23:16:34 +0700

    fix bug render mau den

--- OpenGL/OpenGL/Source.cpp
+++ OpenGL/OpenGL/Source.cpp
@@ -104,32 +104,30 @@ int main() {
          5.0f, -0.5f, -5.0f,  2.0f, 2.0f
     };
 
-    unsigned int cubeVBO, cubeVAO;
-    unsigned int planeVBO, planeVAO;
 
-    glGenBuffers(1, &cubeVBO);
+    unsigned int cubeVBO, cubeVAO;
     glGenVertexArrays(1, &cubeVAO);
+    glGenBuffers(1, &cubeVBO);
     glBindVertexArray(cubeVAO);
     glBindBuffer(GL_ARRAY_BUFFER, cubeVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), cubeVertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(cubeVertices), &cubeVertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
     glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    glGenBuffers(1, &planeVBO);
+    unsigned int planeVBO, planeVAO;
     glGenVertexArrays(1, &planeVAO);
+    glGenBuffers(1, &planeVBO);
     glBindVertexArray(planeVAO);
     glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
-    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 5, (void*)0);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), &planeVertices, GL_STATIC_DRAW);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(float) * 5, (void*)(sizeof(float) * 3));
+    glVertexAttribPointer(0, 3, GL_FLOAT, false, sizeof(float) * 5, (void*)0);
     glEnableVertexAttribArray(1);
+    glVertexAttribPointer(1, 2, GL_FLOAT, false, sizeof(float) * 5, (void*)(sizeof(float) * 3));
     glBindVertexArray(0);
-    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     stbi_set_flip_vertically_on_load(true);
 
@@ -140,37 +138,63 @@ int main() {
     GLenum colorFormat = GL_RGB;
 
     glGenTextures(1, &metal);
+
     metalData = stbi_load("metal.png", &metalWidth, &metalHeight, &metalNRChannels, 0);
-    glActiveTexture(GL_TEXTURE0);
-    glBindTexture(GL_TEXTURE_2D, metal);
-    if (metalNRChannels == 4)
-        colorFormat = GL_RGBA;
-    glTexImage2D(GL_TEXTURE_2D, 0, colorFormat, metalWidth, metalHeight, 0, colorFormat, GL_UNSIGNED_BYTE, metalData);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    stbi_image_free(metalData);
-    colorFormat = GL_RGB;
+    if (metalData) {
+        if (metalNRChannels == 1)
+            colorFormat = GL_RED;
+        if (metalNRChannels == 4)
+            colorFormat = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, metal);
+        glTexImage2D(GL_TEXTURE_2D, 0, colorFormat, metalWidth, metalHeight, 0, colorFormat, GL_UNSIGNED_BYTE, metalData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(metalData);
+        colorFormat = GL_RGB;
+
+    }
+    else {
+        std::cout << "load metal texture failed" << std::endl;
+        stbi_image_free(metalData);
+    }
 
     glGenTextures(1, &marble);
+
     marbleData = stbi_load("marble.jpg", &marbleWidth, &marbleHeight, &marbleNRChannels, 0);
-    glActiveTexture(GL_TEXTURE1);
-    glBindTexture(GL_TEXTURE_2D, marble);
-    if (metalNRChannels == 4)
-        colorFormat = GL_RGBA;
-    glTexImage2D(GL_TEXTURE_2D, 0, colorFormat, marbleWidth, marbleHeight, 0, colorFormat, GL_UNSIGNED_BYTE, marbleData);
-    glGenerateMipmap(GL_TEXTURE_2D);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    stbi_image_free(marbleData);
+    if (marbleData) {
+        if (metalNRChannels == 1)
+            colorFormat = GL_RED; 
+        if (metalNRChannels == 4)
+            colorFormat = GL_RGBA;
+
+        glBindTexture(GL_TEXTURE_2D, marble);
+        glTexImage2D(GL_TEXTURE_2D, 0, colorFormat, marbleWidth, marbleHeight, 0, colorFormat, GL_UNSIGNED_BYTE, marbleData);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        stbi_image_free(marbleData);
+
+    }
+    else {
+        std::cout << "load marble texture failed" << std::endl;
+        stbi_image_free(marbleData);
+
+    }
 
     Shader ourShader("vShader.vert", "fShader.frag");
+    ourShader.use();
+    ourShader.setInt("Image", 0);
 
-    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600, 0.1f, 100.0f);
 
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
@@ -181,23 +205,22 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         ourShader.use();
-
+        glm::mat4 model(1.0f);
         glm::mat4 view = camera.calculateView();
-        ourShader.setMat4("projection", projection);
+        glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600, 0.1f, 100.0f);
         ourShader.setMat4("view", view);
+        ourShader.setMat4("projection", projection);
 
         //render cube 1
         glBindVertexArray(cubeVAO);
-        glm::mat4 model(0.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.25f, 0.0f));
+        model = glm::translate(glm::vec3(0.0f, 0.25f, 0.0f));
         glActiveTexture(GL_TEXTURE0);
         glBindTexture(GL_TEXTURE_2D, metal);
-        ourShader.setInt("Image", 0);
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
 
         //render cube 2
-        model = glm::translate(model, glm::vec3(2.0f, 0.25f, -2.0f));
+        model = glm::translate(glm::vec3(2.0f, 0.25f, -2.0f));
         ourShader.setMat4("model", model);
         glDrawArrays(GL_TRIANGLES, 0, 36);
         glBindVertexArray(0);
@@ -206,9 +229,7 @@ int main() {
         glBindVertexArray(planeVAO);
         model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
         ourShader.setMat4("model", model);
-        glActiveTexture(GL_TEXTURE1);
         glBindTexture(GL_TEXTURE_2D, marble);
-        ourShader.setInt("Image", 1);
         glDrawArrays(GL_TRIANGLES, 0, 6);
         glBindVertexArray(0);
 
--- OpenGL/OpenGL/vShader.vert
+++ OpenGL/OpenGL/vShader.vert
@@ -3,11 +3,11 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec2 aTexCoords;
 
+out vec2 TexCoords;
+
 uniform mat4 projection;
 uniform mat4 view;
 uniform mat4 model;
-
-out vec2 TexCoords;
 
 void main()
 {
